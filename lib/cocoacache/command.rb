d30e8586d8e964edcd1f7b9a427250f3ab671fc6
Suyeol Jeon <devxoul@gmail.com>
2019-06-25 18:25:11 +0900

    Integrate command line

--- lib/cocoacache/command.rb
+++ lib/cocoacache/command.rb
@@ -1,12 +1,16 @@
+require "colored2"
+
 module CocoaCache
   class Command
-    def self.run(argv)
+    def self.run(core_factory, argv)
       case argv[0]
       when "save"
-        CocoaCache::Core.new().save()
+        core = self.get_core(core_factory, argv)
+        core.save()
 
       when "restore"
-        CocoaCache::Core.new().restore()
+        core = self.get_core(core_factory, argv)
+        core.restore()
 
       when "--version"
         puts CocoaCache::VERSION
@@ -16,8 +20,39 @@ module CocoaCache
       end
     end
 
+    def self.get_core(factory, argv)
+      return factory.new(
+        :origin_specs_dir => (
+          self.parse_argument(argv, "--origin") \
+          or "$HOME/.cocoapods/repos/master/Specs"
+        ),
+        :cache_specs_dir => (
+          self.parse_argument(argv, "--cache") \
+            or "Specs"
+        ),
+        :podfile_path => (
+          self.parse_argument(argv, "--podfile") \
+          or "Podfile.lock"
+        ),
+      )
+    end
+
     def self.help
       puts "Usage: cocoacache [save|restore]"
+    end
+
+    def self.parse_argument(argv, name)
+      index = argv.index(name)
+      if index.nil?
+        return nil
+      end
+
+      value = argv[index + 1]
+      if value.nil? or value.start_with?('--')
+        raise Exception("[!] Insufficient value for option '#{name}'".red)
+      end
+
+      return value
     end
   end
 end
---
+++ test/test_command.rb
@@ -0,0 +1,146 @@
+require "minitest/autorun"
+require "rspec/mocks/minitest_integration"
+
+require_relative "../lib/cocoacache"
+
+class CommandTest < Minitest::Test
+
+  def setup()
+    @core = instance_spy(CocoaCache::Core)
+    @factory = class_spy(CocoaCache::Core, :new => @core)
+  end
+
+  def test_save()
+    # when
+    CocoaCache::Command.run(@factory, ["save"])
+
+    # then
+    expect(@core).to have_received(:save)
+    expect(@core).to_not have_received(:restore)
+  end
+
+  def test_save_with_no_arguments__uses_defaults()
+    # when
+    CocoaCache::Command.run(@factory, ["save"])
+
+    # then
+    expect(@factory).to have_received(:new).with(
+      :origin_specs_dir => "$HOME/.cocoapods/repos/master/Specs",
+      :cache_specs_dir => "Specs",
+      :podfile_path => "Podfile.lock",
+    )
+  end
+
+  def test_save_with_arguments()
+    # when
+    CocoaCache::Command.run(@factory, [
+        "save",
+        "--origin", "/awesome/path/to/MyOriginSpecs",
+        "--cache", "/superb/path/to/MyCacheSpecs",
+        "--podfile", "/wonderful/path/to/MyPodfile",
+    ])
+
+    # then
+    expect(@factory).to have_received(:new).with(
+      :origin_specs_dir => "/awesome/path/to/MyOriginSpecs",
+      :cache_specs_dir => "/superb/path/to/MyCacheSpecs",
+      :podfile_path => "/wonderful/path/to/MyPodfile",
+    )
+  end
+
+  def test_save_with_insufficient_arguments()
+    assert_raises Exception do
+      CocoaCache::Command.run(@factory, [
+          "save",
+          "--origin",
+          "--cache", "/superb/path/to/MyCacheSpecs",
+          "--podfile", "/wonderful/path/to/MyPodfile",
+      ])
+    end
+
+    assert_raises Exception do
+      CocoaCache::Command.run(@factory, [
+          "save",
+          "--origin", "/awesome/path/to/MyOriginSpecs",
+          "--cache",
+          "--podfile", "/wonderful/path/to/MyPodfile",
+      ])
+    end
+
+    assert_raises Exception do
+      CocoaCache::Command.run(@factory, [
+          "save",
+          "--origin", "/awesome/path/to/MyOriginSpecs",
+          "--cache", "/superb/path/to/MyCacheSpecs",
+          "--podfile"
+      ])
+    end
+  end
+
+  def test_restore()
+    # when
+    CocoaCache::Command.run(@factory, ["restore"])
+
+    # then
+    expect(@core).to have_received(:restore)
+    expect(@core).to_not have_received(:save)
+  end
+
+  def test_restore_with_no_arguments__uses_defaults()
+    # when
+    CocoaCache::Command.run(@factory, ["restore"])
+
+    # then
+    expect(@factory).to have_received(:new).with(
+      :origin_specs_dir => "$HOME/.cocoapods/repos/master/Specs",
+      :cache_specs_dir => "Specs",
+      :podfile_path => "Podfile.lock",
+    )
+  end
+
+  def test_restore_with_arguments()
+    # when
+    CocoaCache::Command.run(@factory, [
+        "restore",
+        "--origin", "/awesome/path/to/MyOriginSpecs",
+        "--cache", "/superb/path/to/MyCacheSpecs",
+        "--podfile", "/wonderful/path/to/MyPodfile",
+    ])
+
+    # then
+    expect(@factory).to have_received(:new).with(
+      :origin_specs_dir => "/awesome/path/to/MyOriginSpecs",
+      :cache_specs_dir => "/superb/path/to/MyCacheSpecs",
+      :podfile_path => "/wonderful/path/to/MyPodfile",
+    )
+  end
+
+  def test_restore_with_insufficient_arguments()
+    assert_raises Exception do
+      CocoaCache::Command.run(@factory, [
+          "restore",
+          "--origin",
+          "--cache", "/superb/path/to/MyCacheSpecs",
+          "--podfile", "/wonderful/path/to/MyPodfile",
+      ])
+    end
+
+    assert_raises Exception do
+      CocoaCache::Command.run(@factory, [
+          "restore",
+          "--origin", "/awesome/path/to/MyOriginSpecs",
+          "--cache",
+          "--podfile", "/wonderful/path/to/MyPodfile",
+      ])
+    end
+
+    assert_raises Exception do
+      CocoaCache::Command.run(@factory, [
+          "restore",
+          "--origin", "/awesome/path/to/MyOriginSpecs",
+          "--cache", "/superb/path/to/MyCacheSpecs",
+          "--podfile"
+      ])
+    end
+  end
+end
